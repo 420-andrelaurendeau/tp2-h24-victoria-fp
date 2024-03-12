874f1942bdefb56a2e2c0cd9ea960095d784f0f7
victoria-fp <victoria@isbconsultant.com>
2024-03-12 10:30:07 -0400

    Modification de la classe LivreService (création de la fonction createLivre())

--- TP2/src/main/java/ca/cal/tp2/service/LivreService.java
+++ TP2/src/main/java/ca/cal/tp2/service/LivreService.java
@@ -1,4 +1,23 @@
 package ca.cal.tp2.service;
 
+import ca.cal.tp2.modele.Livre;
+import ca.cal.tp2.repository.DocumentRepository;
+
 public class LivreService {
+
+    private final DocumentRepository documentRepository;
+
+    public LivreService(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
+
+    public LivreDTO createLivre(String titre, String auteur) {
+        if (titre == null || auteur == null)
+            throw new NullPointerException("Les paramètres entrés ne peuvent pas être null");
+        if (titre.isEmpty() || auteur.isEmpty())
+            throw new IllegalArgumentException("Le titre et l'auteur' ne peuvent pas être vides");
+
+        Livre livre = documentRepository.saveLivre(titre, auteur);
+        return toDTO(livre);
+    }
 }
