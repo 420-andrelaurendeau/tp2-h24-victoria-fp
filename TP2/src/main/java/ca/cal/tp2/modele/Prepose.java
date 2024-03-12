f8920a68831c3ea3d5e17adb90c52fdad5db37f5
victoria-fp <victoria@isbconsultant.com>
2024-03-12 09:26:38 -0400

    Modification de la classe du modèle Prepose

--- TP2/src/main/java/ca/cal/tp2/modele/Prepose.java
+++ TP2/src/main/java/ca/cal/tp2/modele/Prepose.java
@@ -1,4 +1,24 @@
 package ca.cal.tp2.modele;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("prepose")
 public class Prepose extends Utilisateur {
+    public Prepose() {}
+
+    public Prepose(String nom, String prenom) {
+        super(nom, prenom);
+    }
+
+    @Override
+    public String getNom() {
+        return super.getNom();
+    }
+
+    @Override
+    public String getPrenom() {
+        return super.getPrenom();
+    }
 }
