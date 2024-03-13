f35aff17312f15e5f432634003a67e44ee019395
victoria-fp <victoria@isbconsultant.com>
2024-03-12 20:17:56 -0400

    Implémentation de la fonction saveLivre dans DocumentRepositoryJPA

--- TP2/src/main/java/ca/cal/tp2/repository/DocumentRepositoryJPA.java
+++ TP2/src/main/java/ca/cal/tp2/repository/DocumentRepositoryJPA.java
@@ -1,4 +1,31 @@
 package ca.cal.tp2.repository;
 
+import ca.cal.tp2.modele.Livre;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
 public class DocumentRepositoryJPA implements DocumentRepository {
+
+    public DocumentRepositoryJPA() {}
+
+    @Override
+    public Livre saveLivre(String titre, String auteur, int nbPages) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("tp2victoria");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        final Query query = em.createNativeQuery("select titre, auteur, nbPages from Document where auteur=?");
+        query.setParameter(1, titre);
+
+        Livre livre = new Livre(titre, auteur, nbPages);
+
+        em.persist(livre);
+        em.getTransaction().commit();
+        em.close();
+
+        return livre;
+    }
 }
