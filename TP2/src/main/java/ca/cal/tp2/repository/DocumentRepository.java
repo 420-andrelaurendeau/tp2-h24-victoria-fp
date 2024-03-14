935d6f10752e196b46271fb45a74c45db540d2d3
victoria-fp <victoria@isbconsultant.com>
2024-03-14 14:01:50 -0400

    Déclaration des méthodes findLivreByAuteur(), findLivreByCategorie() et findLivreByTitle() dans l'interface DocumentRepository

--- TP2/src/main/java/ca/cal/tp2/repository/DocumentRepository.java
+++ TP2/src/main/java/ca/cal/tp2/repository/DocumentRepository.java
@@ -6,9 +6,15 @@ public interface DocumentRepository {
 
     Livre saveLivre(String titre, String auteur, int nbExemplairesRestant, int nbPages);
 
+    void updateLivre(Livre livre);
+
     Livre findLivreByTitle(String titre);
 
-    void updateLivre(Livre livre);
+    Livre findLivreByAuteur(String auteur);
+
+    Livre findLivreByCategorie(String categorie);
+
+    Livre findLivreByTitle(int annee);
 
     int getNbExemplairesRestants(Long id);
 }
